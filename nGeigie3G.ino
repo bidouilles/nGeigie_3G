16f2aa4c8612baddacc40c88db0fb564072688dc
Lionel Bergeret <bergeret@pv.com>
2014-06-14 12:07:51 +0900

    Add OpenLog with config file support (NGEIGIE.TXT)

--- nGeigie3G.ino
+++ nGeigie3G.ino
@@ -22,151 +22,185 @@ Connection:
 #include "a3gs.h"
 #include <avr/wdt.h>
 #include <limits.h>
+#include <EEPROM.h>
+#include "nGeigieSetup.h"
+#include "nGeigieDebug.h"
 
 
-  #define LINE_SZ 95
-  //#define PIN1 17 //analog 2 for interrupt for dual tube
-  
-  static char path[LINE_SZ];
-  static char json_buf[LINE_SZ];
-  static char buf[LINE_SZ];
-  static char VERSION[] = "2.2.3_3G";
-  const char *server = "107.161.164.166";
-  const int port = 80;
-  const int interruptMode = RISING;
-  const int updateIntervalInMinutes = 1;
-  const char *apiKey = "q1LKu7RQ8s5pmyxunnDW";
-  const char *lat = "34.4772923" ;
-  const char *lon = "136.1473367" ;
-  const char *ID = "47" ;
-  char res[a3gsMAX_RESULT_LENGTH+1];
-  //char logfile_name[13]; 
-  //bool logfile_ready = false;
-  int len;
-  unsigned long elapsedTime(unsigned long startTime);
-  unsigned long int uSv ;
-  
-  int freeRAM ()
-	{
-		extern int __heap_start, *__brkval;
-		int v;
-		return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-	}
+#define LINE_SZ 95
+//#define PIN1 17 //analog 2 for interrupt for dual tube
 
-    
-   //Setup sdcard from openlog for serial2
-	   SoftwareSerial sdSerial =  SoftwareSerial(10, 13); 
+static char path[LINE_SZ];
+static char json_buf[LINE_SZ];
+static char buf[LINE_SZ];
+static char VERSION[] = "2.2.3_3G";
+const char *server = "107.161.164.166";
+const int port = 80;
+const int interruptMode = RISING;
+const int updateIntervalInMinutes = 1;
+// const char *apiKey = "q1LKu7RQ8s5pmyxunnDW";
+// const char *lat = "34.4772923" ;
+// const char *lon = "136.1473367" ;
+// const char *ID = "47" ;
+char res[a3gsMAX_RESULT_LENGTH+1];
+int len;
+unsigned long elapsedTime(unsigned long startTime);
+unsigned long int uSv ;
 
-	// generate checksum for log format
-		byte len1, chk;     
-		char checksum(char *s, int N)
-			{
-			  int i = 0;
-			  char chk = s[0];
+int freeRAM ()
+{
+	extern int __heap_start, *__brkval;
+	int v;
+	return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+}
 
-			  for (i=1 ; i < N ; i++)
-				chk ^= s[i];
+// OpenLog Settings --------------------------------------------------------------
+//Setup sdcard from openlog for serial2
+SoftwareSerial OpenLog =  SoftwareSerial(10, 13);
+static const int resetOpenLog = A2;
+#define OPENLOG_RETRY 500
+bool openlog_ready = false;
+char logfile_name[13];  // placeholder for filename
+bool logfile_ready = false;
 
-			  return chk;
-			}
-	// initialize the library with the numbers of the interface pins
-	   LiquidCrystal lcd(14,15,8,9,11,12);
-		 
-	// pins used by 3G shield D2, D4,D5,D6,D7 needs to be avoided. 8,9,10,12,13,14,15, can be used. Check D10 for SD card working or not
+// generate checksum for log format
+byte len1, chk;     
+char checksum(char *s, int N)
+{
+  int i = 0;
+  char chk = s[0];
 
-	// Sampling interval (e.g. 60,000ms = 1min)
-		unsigned long updateIntervalInMillis = 0;
+  for (i=1 ; i < N ; i++)
+	chk ^= s[i];
 
-	// The next time to feed
-		unsigned long nextExecuteMillis = 0;
+  return chk;
+}
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(14,15,8,9,11,12);
+ 
+// pins used by 3G shield D2, D4,D5,D6,D7 needs to be avoided. 8,9,10,12,13,14,15, can be used. Check D10 for SD card working or not
 
-	// Event flag signals when a geiger event has occurred
-		volatile unsigned char eventFlag = 0;		// FIXME: Can we get rid of eventFlag and use counts>0?
-		int counts_per_sample;
+// Sampling interval (e.g. 60,000ms = 1min)
+unsigned long updateIntervalInMillis = 0;
 
-	// The last connection time to disconnect from the server
-	// after uploaded feeds
-		long lastConnectionTime = 0;
+// The next time to feed
+unsigned long nextExecuteMillis = 0;
 
-	// The conversion coefficient from cpm to µSv/h
-		float conversionCoefficient = 0;
+// Event flag signals when a geiger event has occurred
+volatile unsigned char eventFlag = 0;		// FIXME: Can we get rid of eventFlag and use counts>0?
+int counts_per_sample;
+
+// The last connection time to disconnect from the server
+// after uploaded feeds
+long lastConnectionTime = 0;
+
+// The conversion coefficient from cpm to µSv/h
+float conversionCoefficient = 0;
+
+// nGeigie Settings --------------------------------------------------------------
+static ConfigType config;
+nGeigieSetup ngeigieSetup(OpenLog, config, buf, LINE_SZ);
 		
-		
-		
+static void setupOpenLog();
+static bool loadConfig(char *fileName);
+static void createFile(char *fileName);
+
 void onPulse()
-	{
+{
 	counts_per_sample++;
 	eventFlag = 1;
-	}
+}
 	
 /**************************************************************************/
 // Setup
 /**************************************************************************/
  
-
-
 void setup()
-	{
-	  wdt_reset();	
-	  Serial.begin(9600);
-	  sdSerial.begin(9600);
-	  Serial.println(VERSION);
-	  //Free ram print
-			  Serial.print(F("Free :\t"));
-			  Serial.println(freeRAM());
-  
-		lcd.begin(8, 2);
-		lcd.clear();
-		lcd.print(F("nGeigie"));
-		lcd.setCursor(0, 1);
-		lcd.print(VERSION);
-	    delay(3000);
-		// Set the conversion coefficient from cpm to µSv/h
-		// LND_7318:
-				Serial.println(F("Sensor Model: LND 7317"));
-				//Serial.println(F("Conversion factor: 344 cpm = 1 uSv/Hr"));
-				conversionCoefficient = 0.0029;
+{
+	wdt_reset();	
+	Serial.begin(9600);
+	OpenLog.begin(9600);
+	Serial.println(VERSION);
 
-		// LND_712:
-				//conversionCoefficient = 0.0083;
-			    //Serial.println(F("Sensor model:   LND 712"));
-			        
-  
-	  //get time
-	  Serial.print(F("Get time. "));
-		lcd.clear();
-		lcd.setCursor(0, 0);
-		lcd.print("Getting");
-		lcd.setCursor(0, 1);
-		lcd.print("time...");
-		delay(3000);
-		  if (a3gs.start() == 0 && a3gs.begin() == 0) 
-			  {
-				Serial.println(F("OK"));
-				char date[a3gsDATE_SIZE], time[a3gsTIME_SIZE];
-				if (a3gs.getTime(date, time) == 0) 
-					{
-					  Serial.print(date);
-					  Serial.println(time);
-					  lcd.setCursor(0, 0);
-					  lcd.print(date);
-					  lcd.setCursor(0, 1);
-					  lcd.print(time);
-					}
-		  else
+	//Free ram print
+	Serial.print(F("Free :\t"));
+	Serial.println(freeRAM());
+
+	lcd.begin(8, 2);
+	lcd.clear();
+	lcd.print(F("nGeigie"));
+	lcd.setCursor(0, 1);
+	lcd.print(VERSION);
+	delay(3000);
+	// Set the conversion coefficient from cpm to µSv/h
+	// LND_7318:
+	Serial.println(F("Sensor Model: LND 7317"));
+	//Serial.println(F("Conversion factor: 344 cpm = 1 uSv/Hr"));
+	conversionCoefficient = 0.0029;
+
+	// LND_712:
+			//conversionCoefficient = 0.0083;
+		    //Serial.println(F("Sensor model:   LND 712"));
+
+	// Load EEPROM settings
+	ngeigieSetup.initialize();
+
+	OpenLog.begin(9600);
+	setupOpenLog();
+	if (openlog_ready) {
+		ngeigieSetup.loadFromFile("NGEIGIE.TXT");
+	}
+	if (!openlog_ready) Serial.println("SD!");
+
+	Serial.println(config.api_key);
+    Serial.println(config.latitude);
+    Serial.println(config.longitude);
+    Serial.println(config.user_id);
+		        
+	//get time
+	Serial.print(F("Get time. "));
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Getting");
+	lcd.setCursor(0, 1);
+	lcd.print("time...");
+	delay(3000);
+	if (a3gs.start() == 0 && a3gs.begin() == 0) 
+	{
+		Serial.println(F("OK"));
+		char date[a3gsDATE_SIZE], time[a3gsTIME_SIZE];
+		if (a3gs.getTime(date, time) == 0) 
+		{
+			Serial.println(date);
+			Serial.println(time);
+			lcd.setCursor(0, 0);
+			lcd.print(date);
+			lcd.setCursor(0, 1);
+			lcd.print(time);
+
+			// Create the filename for that drive
+			sprintf_P(logfile_name, PSTR("%04d%c%c%c%c.log"),config.user_id, date[5], date[6], date[8], date[9]);
+		}
+		else
+		{
 			Serial.println(F("Failed time."));
-		   }
-        
-        //Interrupts setup .. numbers 0 (on digital pin 2) and 1 (on digital pin 3)
-			//attachInterrupt(0, onPulse, interruptMode);                          
-			attachInterrupt(1, onPulse, interruptMode); 
-			
-			
-		//pinMode(PIN1, INPUT); digitalWrite(PIN1, HIGH);
-        //PCintPort::attachInterrupt(PIN1, onPulse, FALLING);                                
-		updateIntervalInMillis = updateIntervalInMinutes * 60000;                  // update time in ms
-		unsigned long now = millis();
-		nextExecuteMillis = now + updateIntervalInMillis;
+			sprintf_P(logfile_name, PSTR("%04d----.log"),config.user_id);
+		}
+	}
+    Serial.println(logfile_name);
+    logfile_ready = true;
+    createFile(logfile_name);
+
+	//Interrupts setup .. numbers 0 (on digital pin 2) and 1 (on digital pin 3)
+		//attachInterrupt(0, onPulse, interruptMode);                          
+		attachInterrupt(1, onPulse, interruptMode); 
+		
+		
+	//pinMode(PIN1, INPUT); digitalWrite(PIN1, HIGH);
+	//PCintPort::attachInterrupt(PIN1, onPulse, FALLING);                                
+	updateIntervalInMillis = updateIntervalInMinutes * 60000;                  // update time in ms
+	unsigned long now = millis();
+nextExecuteMillis = now + updateIntervalInMillis;
 
 		
 		
@@ -200,21 +234,26 @@ void SendDataToServer(float CPM) {
     Serial.println(F("Succeeded."));
     len = sizeof(res);
 
-    sprintf_P(path, PSTR("/scripts/short.php?api_key=%s&lat=%s&lon=%s&cpm=%s&id=%s"),
-      apiKey,
-      lat, \
-      lon, \
+    sprintf_P(path, PSTR("/scripts/short.php?api_key=%s&lat=%s&lon=%s&cpm=%s&id=%d"),
+      config.api_key,
+      config.latitude, \
+      config.longitude, \
       CPM_string, \
-      ID) ;
- 
+      config.user_id) ;
+
+
+ Serial.println(path);
 
     //SD card write prepare
 		memset(buf, 0, LINE_SZ);
-		sprintf_P(buf, PSTR("$BNRDD,%s,,,,%s,,,,%s,,%s,,,,,,"),  \
-				  ID, \
+		sprintf_P(buf, PSTR("$BNRDD,%d,,,,%s,,,,%s,,%s,,,,,,"),  \
+				  config.user_id, \
 				  CPM_string, \
-				  lat,\
-				  lon);
+				  config.latitude,\
+				  config.longitude);
+
+		Serial.println(buf);
+
 				  
 		 len = strlen(buf);
 		  buf[len] = '\0';
@@ -229,7 +268,7 @@ void SendDataToServer(float CPM) {
 			sprintf_P(buf + len, PSTR("*%X"), (int)chk);
 
 	//write to sd card
-       sdSerial.println(buf);
+       OpenLog.println(buf);
 	   //Serial.println(buf);
 	 
 	 
@@ -289,4 +328,80 @@ unsigned long elapsedTime(unsigned long startTime)
 	{
 		return (ULONG_MAX - (startTime - stopTime));
 	}
+}
+
+/**************************************************************************/
+// OpenLog code
+/**************************************************************************/
+
+/* wait for openlog prompt */
+bool waitOpenLog(bool commandMode) {
+  int safeguard = 0;
+  bool result = false;
+
+  while(safeguard < OPENLOG_RETRY) {
+    safeguard++;
+    if(OpenLog.available())
+      if(OpenLog.read() == (commandMode ? '>':'<')) break;
+    delay(10);
+  }
+
+  if (safeguard >= OPENLOG_RETRY) {
+  } else {
+    result = true;
+  }
+
+  return result;
+}
+
+/* setups up the software serial, resets OpenLog */
+void setupOpenLog() {
+  pinMode(resetOpenLog, OUTPUT);
+  OpenLog.listen();
+
+  // reset OpenLog
+  digitalWrite(resetOpenLog, LOW);
+  delay(100);
+  digitalWrite(resetOpenLog, HIGH);
+
+  if (!waitOpenLog(true)) {
+    logfile_ready = true;
+  } else {
+    openlog_ready = true;
+  }
+}
+
+/* create a new file */
+void createFile(char *fileName) {
+  int result = 0;
+  int safeguard = 0;
+
+  OpenLog.listen();
+
+  do {
+    result = 0;
+
+    do {
+      OpenLog.print("append ");
+      OpenLog.print(fileName);
+      OpenLog.write(13); //This is \r
+
+      if (!waitOpenLog(false)) {
+        break;
+      }
+      result = 1;
+    } while (0);
+
+    if (0 == result) {
+      // reset OpenLog
+      digitalWrite(resetOpenLog, LOW);
+      delay(100);
+      digitalWrite(resetOpenLog, HIGH);
+
+      // Wait for OpenLog to return to waiting for a command
+      waitOpenLog(true);
+    }
+  } while (0 == result);
+
+  //OpenLog is now waiting for characters and will record them to the new file
 }
